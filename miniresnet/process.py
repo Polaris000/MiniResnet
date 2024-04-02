91b1e48c5389d2545699c6da335792f37bffdc76
Anchal Agrawal <157994728+anchalagr11@users.noreply.github.com>
2024-04-02 10:31:06 -0400

    Update process.py

--- miniresnet/process.py
+++ miniresnet/process.py
@@ -1,12 +1,45 @@
+import torch
+import torchvision.transforms as transforms
+from torchvision import datasets
+
 def load_data():
+
+    transform_train, transform_test = augment_data()
+    
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+    
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
     # return all train loaders
     # call augment_data here.
-
     # ensure data is saved in data
-    pass
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck')
+    
+    return trainloader, testloader, classes
+# load data
+trainloader, testloader, classes = load_data()
+
 
 def augment_data():
-    pass
+
+    transform_train = transforms.Compose([
+        transforms.RandomVerticalFlip(),
+        # transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+        transforms.RandomCrop(32, padding=4),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+    return transform_train, transform_test
+
 
 """
 
